0fc80a9aa6ad02259ff4a9eb4949bf6f68d1405d
NooraAlrashid3 <nooraalrashid3322@gmail.com>
2023-02-07 11:55:18 +0300

    versions file

--- 2-backend.tf
+++ 2-backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "aws-arch"
+    organization = "aws-noora-terraform"
 
     workspaces {
       name = "01-lab7-vpc"
